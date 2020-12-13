06d67d91b7da999cc93d97a3252854a6067b1162
chris covarrubias <chris.covarrubias40@gmail.com>
2020-12-13 15:38:42 -0800

    budget-tracker

--- Develop/public/service-worker.js
+++ Develop/public/service-worker.js
@@ -3,8 +3,10 @@ const DATA_CACHE_NAME = "data-cache-v1";
 const FILES_TO_CACHE = [
     "/",
     "/index.html",
-    "icon-192x192.png",
-    "icon-512x512.png"
+    "/public/icons/icon-192x192.png",
+    "/public/icons/icon-512x512.png",
+    "/public/styles.css"
+
 ]
 
 
